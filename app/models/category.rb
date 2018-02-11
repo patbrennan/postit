c6f1630aaf07eccf09c18e21bc87e4228d3a833e
patbrennan <patrick@junecommerce.org>
2018-02-11 17:35:43 +0000

    edit slugging to be more robust

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,12 +4,30 @@ class Category < ActiveRecord::Base
   
   validates :name, presence: true, uniqueness: true, length: {minimum: 2}
   
-  before_save :generate_slug
+  before_save :generate_slug!
   
-  def generate_slug
-    new_slug = self.name.gsub(/[\s[^a-zA-Z0-9]]/, "-").downcase
-    new_slug = new_slug.gsub(/-{2,}/, "-")
+  def generate_slug!
+    return if self.slug
+    
+    new_slug = to_slug(self.name)
+    last_char = new_slug[-1]
+    
+    while Category.find_by(slug: new_slug)
+      if last_char.to_i == 0
+        new_slug += "-" + (last_char.to_i + 1).to_s
+      else
+        next_num = last_char.to_i + 1
+        new_slug[-1] = next_num.to_s
+      end
+    end
     self.slug = new_slug
+  end
+  
+  def to_slug(name)
+    slugged = name.gsub(/[\s[^a-zA-Z0-9]]/, "-")
+    slugged.gsub!(/-{2,}/, "-")
+    slugged.gsub!(/\A-+|-+\z/, "")
+    slugged.downcase
   end
   
   def to_param
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,9 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true
+  validates :slug, uniqueness: true
   
-  before_save :generate_slug
+  before_save :generate_slug!
   
   def vote_count
     self.up_votes - self.down_votes
@@ -23,10 +24,49 @@ class Post < ActiveRecord::Base
     self.votes.where(vote: false).size
   end
   
-  def generate_slug
-    new_slug = self.title.gsub(/[\s[^a-zA-Z0-9]]/, "-").downcase
-    new_slug = new_slug.gsub(/-{2,}/, "-")
+  # def generate_slug
+  #   new_slug = to_slug(self.name)
+  #   post = Post.find_by(slug: new_slug)
+  #   count = 2
+    
+  #   while post && post != self
+  #     new_slug = append_suffix(new_slug, count)
+  #     post = Post.find_by(slug: new_slug)
+  #     count += 1
+  #   end
+  #   self.slug = new_slug
+  # end
+  
+  # def append_suffix(str, count)
+  #   if str.split("-").last.to_i != 0
+  #     return str.split("-").slice(0...-1).join("-") + "-" + count.to_s
+  #   else
+  #     return str + "-" + count.to_s
+  #   end
+  # end
+  
+  def generate_slug!
+    return if self.slug
+    
+    new_slug = to_slug(self.title)
+    last_char = new_slug[-1]
+    
+    while Post.find_by(slug: new_slug)
+      if last_char.to_i == 0
+        new_slug += "-" + (last_char.to_i + 1).to_s
+      else
+        next_num = last_char.to_i + 1
+        new_slug[-1] = next_num.to_s
+      end
+    end
     self.slug = new_slug
+  end
+  
+  def to_slug(name)
+    slugged = name.gsub(/[\s[^a-zA-Z0-9]]/, "-")
+    slugged.gsub!(/-{2,}/, "-")
+    slugged.gsub!(/\A-+|-+\z/, "")
+    slugged.downcase
   end
   
   def to_param
